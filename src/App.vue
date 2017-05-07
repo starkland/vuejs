1b494cbae9737d4794f381be48dfc41ba3a31656
thulioph <thulioph@gmail.com>
2017-05-07 00:25:23 -0300

    First commit.

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <st-header></st-header>
+</template>
+
+<script>
+import stHeader from './components/Header.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    stHeader
+  },
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,91 @@
+<template>
+  <nav class="nav">
+    <div class="nav-left">
+      <a
+        class="nav-item"
+        target="_blank"
+        title="Go to Vuejs site"
+        href="http://vuejs.org/">
+        <img
+          src="http://vuejs.org/images/logo.png"
+          alt="Vuejs logo">
+      </a>
+    </div>
+
+    <div class="nav-center">
+      <a
+        class="nav-item"
+        href="https://github.com/starkland/vuejs"
+        target="_blank">
+        <span class="icon">
+          <i class="fa fa-github"></i>
+        </span>
+      </a>
+      <a
+        class="nav-item"
+        href="https://twitter.com/thulioph_"
+        target="_blank">
+        <span class="icon">
+          <i class="fa fa-twitter"></i>
+        </span>
+      </a>
+    </div>
+
+    <span
+      class="nav-toggle"
+      :class="{ 'is-active': isOpen }"
+      @click="toggleNavbar">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+
+    <!-- This "nav-menu" is hidden on mobile -->
+    <!-- Add the modifier "is-active" to display it on mobile -->
+    <div
+      class="nav-right nav-menu"
+      :class="{ 'is-active': isOpen }">
+      <a class="nav-item is-tab">
+        Home
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+
+  methods: {
+    toggleNavbar() {
+      this.isOpen = !this.isOpen;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .nav {
+    background-color: #363636;
+  }
+
+
+  .nav a:hover {
+    color: #FFFFFF;
+  }
+
+  .nav-toggle.is-active {
+    background-color: #FFF;
+  }
+
+  .nav-right a:hover {
+    background-color: #FFFFFF;
+    color: #363636 !important;
+  }
+</style>
