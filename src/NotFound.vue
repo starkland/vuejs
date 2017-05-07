9cfcfbe03e2ec875137e7badf2aa8cc18d2dafd9
thulioph <thulioph@gmail.com>
2017-05-07 02:05:41 -0300

    Adds the footer on Not found component.

--- src/NotFound.vue
+++ src/NotFound.vue
@@ -7,19 +7,23 @@
       :title="header.title"
       :subtitle="header.subtitle">
     </st-sub-header>
+
+    <st-footer></st-footer>
   </div>
 </template>
 
 <script>
 import stHeader from './components/Header.vue';
 import stSubHeader from './components/CallToAction.vue';
+import stFooter from './components/Footer.vue';
 
 export default {
   name: 'NotFound',
 
   components: {
     stHeader,
-    stSubHeader
+    stSubHeader,
+    stFooter
   },
 
   data() {
