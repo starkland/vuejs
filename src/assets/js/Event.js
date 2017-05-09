b83035870d47bdb79820b5b6bab8a607f98019c2
Thulio Philipe <thulioph@gmail.com>
2017-05-08 21:01:00 -0300

    Refactors the event file.

--- src/assets/js/Event.js
+++ src/assets/js/Event.js
@@ -1,20 +1,9 @@
 import Vue from 'vue';
 
-class Event {
-  constructor(name) {
-    this.evt_name = name;
-    this.event = new Vue();
-  }
+// ====
 
-  send(evt_data) {
-    return this.event.$emit(this.evt_name, evt_data);
-  }
+const Event = new Vue();
 
-  listen() {
-    return this.event.$on(this.evt_name);
-  }
+// ====
 
-  turnOff() {
-    return this.event.$off(this.evt_name);
-  }
-}
+export default Event;
